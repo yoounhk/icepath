cfca79713b608cb591035cce7d2af3e195ba74f0
yhk <ezenyhk@gmail.com>
2020-09-21 17:32:43 +0900

    Refactor If expression to when

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,30 +4,21 @@ import java.util.*
 fun main(args: Array<String>) {
 
     var game = Game()
-
+    
     var key = '0'
     var sc = Scanner(System.`in`)
     while (key != 'q') {
-        if (game.isWin()) {
-            println("승리")
-            return
-        }
-        game.draw()
+        game.draw() // 움직일때 마다 draw() 하기
         println()
         print("input(quit: q, reset: r) > ")
         key = sc.nextLine()[0] // TODO: 입력 좀 더 스마트하게 받기
-        if (key == 'w') { // TODO: 코드 분리하기
-            game.moveUp()
-        } else if (key == 's') {
-            game.moveDown()
-        } else if (key == 'a') {
-            game.moveLeft()
-        } else if (key == 'd') {
-            game.moveRight()
-        } else if (key == 'r') { // TODO 초기화 깔끔하게 바꾸기
-            game = Game()
-        } else {
-            println("invalid input detected: $key")
+        when (key) {
+            'w' -> game.moveUp()
+            's' -> game.moveDown()
+            'a' -> game.moveLeft()
+            'd' -> game.moveRight()
+            'r' -> game = Game() // TODO Game.reset() 만들기
+            else -> println("invalid input detected: $key")
         }
     }
 }
